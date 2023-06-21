fc182f7444e7d1fd213cda1a944ff23f1f56d049
Raghav Patne <rapat@dribe.dk>
2023-06-21 08:41:45 +0200

    added assignment and finished section 4

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const http = require('http');
 const routes = require('./routes');
 
 console.log(routes.someText);
+console.log('nodemon started')
 
 const server = http.createServer(routes.handler);
 
