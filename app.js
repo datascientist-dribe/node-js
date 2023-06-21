847716c8ca0cd9c2cdd936a71e871b8e7037d760
Raghav Patne <rapat@dribe.dk>
2023-06-21 09:29:15 +0200

    added express request parsing

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 const http = require('http');
-const routes = require('./routes');
 
-console.log(routes.someText);
-console.log('nodemon started')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer(routes.handler);
+const app = express();
 
-server.listen(3000);
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+    console.log('in add products')
+    res.send('<form action="/product" method="POST"><input type = "text" name="title"><button type="submit">Add product</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    console.log('in the server');
+    next();
+});
+
+
+
+app.listen(3000);
+
+// const server = http.createServer(app);
+
+// server.listen(3000);
